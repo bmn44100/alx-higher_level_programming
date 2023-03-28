5806bd6522bce3f184bdb2012893df6ce69aab9d
bmn44100 <njerumurimi@gmail.com>
2023-03-28 13:09:11 -0700

    class Square that defines a square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
+""" defines a square """
 
 
 class Square:
-    """
-    creates square
-    """
+    """creates square"""
 
     def __init__(self, size=0):
         """
@@ -17,16 +16,12 @@ class Square:
 
     @property
     def size(self):
-        """
-        finds size
-        """
+        """finds size"""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """
-        validates size is an integer that is greater than zero
-        """
+        """validates size is an integer that is greater than zero"""
         if type(value) is not int:
             raise TypeError('size must be an integer')
         elif value < 0:
@@ -44,9 +39,7 @@ class Square:
         return self.__size ** 2
 
     def my_print(self):
-        """
-        prints square with character #
-        """
+        """prints square with character #"""
         if self.__size == 0:
             print("")
             return
