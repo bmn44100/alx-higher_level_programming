61682f5ec912ed1a64d6a3f5772decdb3f8bde79
bmn44100 <njerumurimi@gmail.com>
2023-04-17 13:51:23 -0700

    class Rectangle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" module that contains the class rectangle """
+
+
+class Rectangle(Base):
+    """ class Rectangle """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """ constructor for class Rectangle """
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
