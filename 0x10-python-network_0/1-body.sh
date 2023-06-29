b3e30a69d6a8f4fa2019a2448f931ab56e10ec25
bmn44100 <njerumurimi@gmail.com>
2023-06-29 12:56:10 -0700

    a Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # a Bash script that takes in a URL,
-# sends a request to that URL
-# and displays the size of the body of the response
+# sends a GET request to the URL
+# and displays the body of the response
 curl -sL "$1"
