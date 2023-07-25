bd11c1277c1856d59e47d525234abfbbd55f3d74
bmn44100 <njerumurimi@gmail.com>
2023-07-25 20:53:28 +0000

    a script that reads and prints the content of a file.

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-/*
-  a script that reads and prints the content of a file
-*/
+
 let fs = require('fs');
 let file_path = process.argv[2];
 fs.readFile(file_path, 'utf-8', (err, content) => {
